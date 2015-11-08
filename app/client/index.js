aa4b61df83ae41d725b5ccef184a9d1ecf8f05b8
essem <soomin@gmail.com>
2015-11-08 15:46:14 +0900

    Bundle bootstrap by webpack

--- app/client/index.js
+++ app/client/index.js
@@ -1,3 +1,4 @@
+require("./bootstrap/css/bootstrap.css");
 import React from 'react';
 import ReactDOM from 'react-dom';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 var path = require('path');
 
 module.exports = {
-    entry: "./app/client/index.js",
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: "bundle.js",
-        publicPath: "http://localhost:5001/assets/"
-    },
-    module: {
-        loaders: [
-          { test: /\.jsx?$/, loader: "babel", exclude: /node_modules/ }
-        ]
-    }
+  entry: "./app/client/index.js",
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: "bundle.js",
+    publicPath: "http://localhost:5001/assets/"
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: "babel", exclude: /node_modules/ },
+      { test: /\.css$/, loader: "style!css", exclude: /node_modules/ },
+      { test: /\.woff2?(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&minetype=image/svg+xml" }
+    ]
+  }
 };
